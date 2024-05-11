a5720d537ada50ef676a53b59d02d81753fa3e23
Fabletownn <fable_town@hotmail.com>
2024-05-10 21:25:27 -0400

    Setup

--- src/main/java/com/bmdynamiterestriction/BMDynamiteRestrictionConfig.java
+++ src/main/java/com/bmdynamiterestriction/BMDynamiteRestrictionConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.bmdynamiterestriction;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface BMDynamiteRestrictionConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
--- src/test/java/com/bmdynamiterestriction/BMDynamiteRestrictionPlugin.java
+++ src/test/java/com/bmdynamiterestriction/BMDynamiteRestrictionPlugin.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.bmdynamiterestriction;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class BMDynamiteRestrictionPlugin
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(BMDynamiteRestrictionPlugin.class);
 		RuneLite.main(args);
 	}
 }
