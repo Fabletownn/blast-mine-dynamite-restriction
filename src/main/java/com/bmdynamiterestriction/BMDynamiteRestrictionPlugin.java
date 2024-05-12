0f9e346362f70d1c8ce9400fff5be10a05d933f5
Fabletownn <fable_town@hotmail.com>
2024-05-12 15:01:17 -0400

    simplifying logic, fixing issues, added icon

--- src/main/java/com/bmdynamiterestriction/BMDynamiteRestrictionPlugin.java
+++ src/main/java/com/bmdynamiterestriction/BMDynamiteRestrictionPlugin.java
@@ -5,11 +5,12 @@ import javax.inject.Inject;
 
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
+import net.runelite.api.coords.LocalPoint;
+import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.events.*;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
-import net.runelite.api.Item;
 import net.runelite.api.ItemContainer;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
@@ -30,24 +31,59 @@ public class BMDynamiteRestrictionPlugin extends Plugin
 	@Inject
 	private BMDynamiteRestrictionConfig config;
 
-	// IDs
 	private static final int HARD_ROCK_ID = 28579;
 	private static final int HARD_ROCK_ID_2 = 28580;
 	private static final int UNNOTED_DYNAMITE_ID = 13573;
 
-	// Notifications
-	private boolean outOfDynamiteNotified = false;
-	private boolean hasDynamiteNotified = false;
+	private boolean hadDynamite = false;
+	private boolean hasDynamite = false;
+	private boolean atBank = false;
 
-	// Inventory
 	private ItemContainer previousInventory = null;
 
+	private static final String CHISEL_OPTION = "Excavate";
+	private static final String NO_DYNAMITE_MSG = "You don't have any dynamite with which to load the cavity.";
+	private static final String REPLENISH_DYNAMITE_MSG = "You now have dynamite to chisel Hard Rock once more.";
+
 	@Subscribe
-	protected void onGameStateChanged(GameStateChanged state)
+	public void onGameTick(GameTick event)
 	{
-		if (state.getGameState() == GameState.LOGGED_IN)
+		ItemContainer currentInventory = client.getItemContainer(InventoryID.INVENTORY);
+
+		if (!isPlayerInBlastMine())
 		{
-			clientThread.invokeLater(() -> previousInventory = client.getItemContainer(InventoryID.INVENTORY));
+			return;
+		}
+
+		if (previousInventory != null && currentInventory != null)
+		{
+			boolean prevDynamite = hadDynamite;
+
+			clientThread.invoke(() -> {
+				hadDynamite = previousInventory.contains(UNNOTED_DYNAMITE_ID);
+
+				hasDynamite = previousInventory.contains(UNNOTED_DYNAMITE_ID);
+			});
+
+			if (!prevDynamite && hasDynamite)
+			{
+				if (hasReplenishedDynamiteMessagesEnabled())
+				{
+					client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", REPLENISH_DYNAMITE_MSG, null);
+				}
+			}
+			else if (prevDynamite && !hasDynamite && !atBank)
+			{
+				if (hasOutOfDynamiteMessagesEnabled())
+				{
+					client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", NO_DYNAMITE_MSG, null);
+				}
+
+				if (config.soundfx())
+				{
+					playOutOfDynamiteEffect();
+				}
+			}
 		}
 	}
 
@@ -56,22 +92,20 @@ public class BMDynamiteRestrictionPlugin extends Plugin
 	{
 		ItemContainer currentInventory = client.getItemContainer(InventoryID.INVENTORY);
 
-		if (currentInventory == null) return;
-		if (entry.getOption().equals("Excavate") && !checkUnnotedDynamite(currentInventory) && (entry.getIdentifier() == HARD_ROCK_ID || entry.getIdentifier() == HARD_ROCK_ID_2))
+		if (!isPlayerInBlastMine())
 		{
-			if (config.depmenu())
-			{
-				entry.getMenuEntry().setDeprioritized(true);
-			}
+			return;
+		}
 
-			if (hasOutOfDynamiteMessagesEnabled() && !outOfDynamiteNotified)
+		if (currentInventory != null)
+		{
+			if (entry.getOption().equals(CHISEL_OPTION) && !currentInventory.contains(UNNOTED_DYNAMITE_ID) && (entry.getIdentifier() == HARD_ROCK_ID || entry.getIdentifier() == HARD_ROCK_ID_2))
 			{
-				client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "You ran out of dynamite and cannot chisel Hard Rock.", null);
-				playOutOfDynamiteEffect();
+				if (config.depmenu())
+				{
+					entry.getMenuEntry().setDeprioritized(true);
+				}
 			}
-
-			outOfDynamiteNotified = true;
-			hasDynamiteNotified = false;
 		}
 	}
 
@@ -79,49 +113,26 @@ public class BMDynamiteRestrictionPlugin extends Plugin
 	public void onItemContainerChanged(ItemContainerChanged container)
 	{
 		ItemContainer playerInventory = client.getItemContainer(InventoryID.INVENTORY);
+		ItemContainer playerBank = client.getItemContainer(InventoryID.BANK);
 		ItemContainer changedContainer = container.getItemContainer();
 
-		if (previousInventory == null)
+		if (!isPlayerInBlastMine())
 		{
-			previousInventory = changedContainer;
+			return;
 		}
-		else if (playerInventory != null && changedContainer != null)
+
+		if (changedContainer != null && playerBank != null)
+		{
+			atBank = changedContainer == playerBank;
+		}
+
+		if (changedContainer != null && playerInventory != null)
 		{
 			if (changedContainer.getId() == playerInventory.getId())
 			{
-				boolean hadDynamite = checkUnnotedDynamite(previousInventory);
-				boolean hasDynamite = checkUnnotedDynamite(playerInventory);
-
-				log.info("Previous: {} Player: {}", hadDynamite, hasDynamite);
-
-				if (!hadDynamite && hasDynamite)
-				{
-					if (hasReplenishedDynamiteMessagesEnabled() && !hasDynamiteNotified)
-					{
-						client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "You now have dynamite to chisel Hard Rock once more.", null);
-					}
-
-					outOfDynamiteNotified = false;
-					hasDynamiteNotified = true;
-				}
-			}
-			previousInventory = changedContainer;
-		}
-	}
-
-	private boolean checkUnnotedDynamite(ItemContainer container)
-	{
-		if (container != null)
-		{
-			for (Item item : container.getItems())
-			{
-				if (item.getId() == UNNOTED_DYNAMITE_ID)
-				{
-					return true;
-				}
+				previousInventory = playerInventory;
 			}
 		}
-		return false;
 	}
 
 	private void playOutOfDynamiteEffect()
@@ -146,6 +157,17 @@ public class BMDynamiteRestrictionPlugin extends Plugin
 
 		return messagesEnabled == BMDynamiteRestrictionConfig.MessagesEnabledType.ALL_MESSAGES ||
 				messagesEnabled == BMDynamiteRestrictionConfig.MessagesEnabledType.REPLENISHED_DYNAMITE;
+	}
+
+	private boolean isPlayerInBlastMine()
+	{
+		LocalPoint playerLocation = client.getLocalPlayer().getLocalLocation();
+
+		int playerLocationX = WorldPoint.fromLocalInstance(client, playerLocation).getX();
+		int playerLocationY = WorldPoint.fromLocalInstance(client, playerLocation).getY();
+
+		return playerLocationX >= 1465 && playerLocationX <= 1515 &&
+			   playerLocationY >= 3840 && playerLocationY <= 3890;
 	}
 
 	@Provides
